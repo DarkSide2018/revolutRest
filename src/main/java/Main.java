a662bfcd898df5ac020a19d31f337649df801220
DefNeoRoman <d89056411060@yandex.ru>
2018-10-26 20:23:01 +0300

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,26 @@
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
+import servlets.BankServlet;
+
+import java.util.logging.Logger;
+
+public class Main {
+    private static Logger logger = Logger.getLogger(Main.class.getName());
+    public static void main(String[] args) {
+        BankServlet bankServlet = new BankServlet();
+        ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
+        context.addServlet(new ServletHolder(bankServlet), "/bank");
+        Server server = new Server(8080);
+        server.setHandler(context);
+
+        try {
+            server.start();
+            logger.info("Server starting");
+            server.join();
+        } catch (Exception e) {
+            logger.warning("Problem with start");
+        }
+
+    }
+}
